1c9b8322c7889eed5956370c92824bd64c496888
protic-ss <nitesh.minz@softsuave.org>
2023-11-03 22:48:46 +0530

    docker included and front and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./main/home";
+import "./App.css";
+import Login from "./main/login";
+import SignUp from "./main/signUp";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SignUp />}></Route>
+      <Route path="home" element={<Home />}></Route>
+      <Route path="login" element={<Login />}></Route>
+    </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
