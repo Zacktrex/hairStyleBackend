41e72c0f03ef3b5af8c38e1fb7bd1031463c0506
Zacktrex <gameonplayon71@gmail.com>
2024-01-13 15:23:52 +0530

    update

--- frontend/src/main/header/Header.jsx
+++ frontend/src/main/header/Header.jsx
@@ -73,10 +73,10 @@ function Nav() {
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <a
-                    href="#"
+                    href="home"
                     className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Dashboard
+                   Hair Style
                   </a>
 
                   <a
